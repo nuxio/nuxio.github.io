f44cb45ccf2ca5060e6abcfb1a28cd263090e9d0
nuxio <634801009@qq.com>
2019-01-03 00:25:51 +0800

    Happy new year - 2019

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Another blog.
+
+## Start
+```bash
+$ bundle exec jekyll serve
+```
+    Tips:
+    If something is wrong, delete Gemfile.lock and retry.
+
+## Before Commit
+```bash
+$ node ./_scripts/create-manifest.js
+```
--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 var CACHE_NAME = 'my-blog-cache-v1'
 // Read only
-var urlsToCache = ["/","/archives/","/about/","/2017/12/15/welcome-to-jekyll.html","/2018/01/01/happy-new-year.html","/2018/09/09/debounce-and-throttle.html","/404.html","/README.md","/about/index.html","/app.js","/archives/index.html","/assets/fonts/sourcesanspro.woff","/assets/fonts/sourcesanspro.woff2","/atom.xml","/css/main.css","/favicon.png","/index.html","/manifest.json","/sw.js"]
+var urlsToCache = ["/","/archives/","/about/","/2017/12/15/welcome-to-jekyll.html","/2018/01/01/happy-new-year.html","/2018/09/09/debounce-and-throttle.html","/2019/01/01/happy-new-year.html","/404.html","/README.md","/about/index.html","/app.js","/archives/index.html","/assets/fonts/sourcesanspro.woff","/assets/fonts/sourcesanspro.woff2","/atom.xml","/css/main.css","/favicon.png","/index.html","/manifest.json","/sw.js"]
 
 self.addEventListener('install', function (event) {
   // install
